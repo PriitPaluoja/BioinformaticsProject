e0d7773e399af45894b3aaa73b92b4d63eb8a6ee
Priit Paluoja <priitpaluoja@gmail.com>
2019-05-24 13:09:03 +0300

    Create next2.sh

---
+++ next2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --time=20:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=priitpaluoja@gmail.com
+#SBATCH -J PIPELINE2
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=1000
+#SBATCH --cpus-per-task=1
+
+module load python-3.6.3
+module load samtools-1.9
+module load htslib-1.9
+module load jdk-1.8.0_25
+
+source activate bio
+
+export bazam="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/bazam.jar"
+
+hisat_db="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/annotations/Homo_sapiens.GRCh38.dna.primary_assembly"
+fasta_ref="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/annotations/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
+crams="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/cram/"
+
+
+nextflow pipeline2.nf --hisat2_index $hisat_db --fasta_ref $fasta_ref --crams $crams
