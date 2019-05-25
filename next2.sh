bde1ec903052fa2e7c99ac02cb6f6360cd5b8df9
Priit Paluoja <priitpaluoja@gmail.com>
2019-05-25 09:32:43 +0300

    Less memory

--- next2.sh
+++ next2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=20:00:00
+#SBATCH --time=25:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=priitpaluoja@gmail.com
 #SBATCH -J PIPELINE2
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=1000
+#SBATCH --mem=500
 #SBATCH --cpus-per-task=1
 
 module load python-3.6.3
@@ -19,7 +19,7 @@ export bazam="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProje
 
 hisat_db="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/annotations/Homo_sapiens.GRCh38.dna.primary_assembly"
 fasta_ref="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/annotations/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
-crams="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/cram/"
+crams="/gpfs/hpchome/ppaluoja/bioinformaatika/projekt/BioinformaticsProject/cram/test/"
 
 
 nextflow pipeline2.nf --hisat2_index $hisat_db --fasta_ref $fasta_ref --crams $crams
